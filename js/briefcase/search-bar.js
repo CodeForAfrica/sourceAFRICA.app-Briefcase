f214960cdfe17f4521d0f6a898069664937ba857
David Lemayian <dlemayian@gmail.com>
2016-05-02 22:28:21 +0300

    Fix FB app ID

--- js/briefcase/search-bar.js
+++ js/briefcase/search-bar.js
@@ -21,7 +21,7 @@ $('document').ready(function(){
 BC.fn.search_bar.setShareLinks = function () {
   var share_msg = encodeURIComponent('"' + BC.search.text + '" in Briefcase - ' + window.location.href);
   var share_tw  = 'https://twitter.com/intent/tweet?text=' + share_msg + '&via=Code4Africa';
-  var share_fb  = 'https://www.facebook.com/dialog/share?app_id=1485137868446481&display=popup';
+  var share_fb  = 'https://www.facebook.com/dialog/share?app_id=772534026119230&display=popup';
       share_fb += '&href=' + encodeURIComponent(window.location.href);
       share_fb += '&redirect_uri=' + encodeURIComponent(window.location.href);
   

09a787c9cc59cffafb6ca6e94a7863013ac619e3
David Lemayian <david@on.co.ke>
2016-05-09 17:01:08 +0300

    Trim anything searched.

--- js/briefcase/search-bar.js
+++ js/briefcase/search-bar.js
@@ -5,7 +5,7 @@ BC.fn.search_bar = {};
 $('document').ready(function(){
 
   $('#search-btn').click(function(){
-    window.location = "/search.html#q=" + encodeURIComponent($('#search').val());
+    window.location = "/search.html#q=" + encodeURIComponent($('#search').val().trim());
   });
 
   $('#search').keypress(function(e){
